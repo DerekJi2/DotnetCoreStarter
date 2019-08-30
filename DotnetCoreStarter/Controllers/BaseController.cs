d6ce9934439768af20667d5c648940150652c6c6
Derek Ji <syfool@hotmail.com>
2019-08-30 21:00:27 +0930

    ActionFilters & DI & Cors

---
+++ DotnetCoreStarter/Controllers/BaseController.cs
@@ -0,0 +1,24 @@
+﻿using DotnetCoreStarter.Utilities.ActionFilters;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotnetCoreStarter.Controllers
+{
+    [TypeFilter(typeof(NLogActionFilter))]
+    [TypeFilter(typeof(AuthActionFilter))]
+    public abstract class BaseController : Controller
+    {
+        protected readonly ILogger<ILogActionFilter> _logger;
+
+        public BaseController(
+            ILogger<ILogActionFilter> logger
+            )
+        {
+            _logger = logger;
+        }
+    }
+}
---
+++ DotnetCoreStarter/Startup.InjectionModule.cs
@@ -0,0 +1,20 @@
+﻿using DotnetCoreStarter.Utilities.ActionFilters;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotnetCoreStarter
+{
+    public partial class Startup
+    {
+        public void InejctInfrastructures(IServiceCollection services)
+        {
+            services.AddTransient<ILogActionFilter, NLogActionFilter>();
+            services.AddTransient<IAuthActionFilter, AuthActionFilter>();
+
+        }
+
+    }
+}
